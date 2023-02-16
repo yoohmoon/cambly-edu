59ce6f93b671463f25a0ccfdc6e9d287de34a80c
yoohmoon <ansekfzhd5455@naver.com>
2023-02-17 00:29:30 +0900

    슬라이드 구현중

--- src/components/home/CurriSection.jsx
+++ src/components/home/CurriSection.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { TbBooks } from "react-icons/tb";
 import CurriBook from "./CurriBook";
 import { useEffect, useRef, useState } from "react";
@@ -60,6 +60,56 @@ const bookInfo = [
     count: "9 lessons",
     title: "Academic English",
     src: "https://camblycurriculumicons.s3.amazonaws.com/5fa1de81b728c84f4ccaaa2f?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+  {
+    id: 1,
+    level: "Basic",
+    count: "10 lessons",
+    title: "Basic Conversation Topics",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e2b895e541a832674533c18?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+  {
+    id: 2,
+    level: "Intermediate",
+    count: "9 lessons",
+    title: "Life in the Internet Age",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e0e8b212ac750e7dc9886ac?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+  {
+    id: 3,
+    level: "Intermediate",
+    count: "10 lessons",
+    title: "Intermediate Conversation Topics",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e2b99d0c4288f294426b643?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+
+  {
+    id: 4,
+    level: "Advanced",
+    count: "10 lessons",
+    title: "Advanced Conversation Topics",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e2b99e60b114e9a327ceb66?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+  {
+    id: 5,
+    level: "Intermediate",
+    count: "7 lessons",
+    title: "Caring for Our Planet",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e2b99f70f8f1e9f625e8317?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+  {
+    id: 6,
+    level: "Intermediate",
+    count: "6 lessons",
+    title: "Healthy Mind, Healthy Body",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e2b9a4c05342470fdddf8b8?h=d41d8cd98f00b204e9800998ecf8427e",
+  },
+  {
+    id: 7,
+    level: "Intermediate",
+    count: "10 lessons",
+    title: "Business English",
+    src: "https://camblycurriculumicons.s3.amazonaws.com/5e2b9a5feb6295be78ddf8c3?h=d41d8cd98f00b204e9800998ecf8427e",
   },
 ];
 
@@ -128,6 +178,19 @@ function CurriSection() {
   );
 }
 
+const slide = keyframes`
+ /* from{
+  transform: translate(-80px);
+ } */
+  
+  to {
+    transform: translate(-840px);
+    /* transform: translate(-310px); */
+
+    /* id2로 끝나고 id3가 처음으로 와야함 */
+  }
+`;
+
 const Container = styled.div`
   min-height: 600px;
   background-color: ${({ theme }) => theme.colors.main_blue};
@@ -150,22 +213,28 @@ const TitleWrapper = styled.div`
 
 const BookContainer = styled.div``;
 
-const CurriWrapper = styled.div``;
-
-const BookWrapper = styled.div`
+const CurriWrapper = styled.div`
+  /* margin: 0 auto; */
+  overflow: hidden;
   border: 1px solid #000;
   border-radius: 12px;
   background-color: #fff;
   max-width: 1380px;
+`;
 
-  overflow: hidden;
-
+const BookWrapper = styled.div`
+  /* transform: translate(300px); */
+  transform: translate(200px);
   display: flex;
   justify-content: center;
   /* align-items: center; */
   gap: 20px;
+
+  animation: ${slide} 8s linear infinite;
 `;
 
-// (180 * 7)+ (20 * 6) =
+// (180 * 7)+ (20 * 6) = 1380px
+
+// (container : 1380px) + (item width length : 180)  + [gap?? 20px??] =1560 +[]
 
 export default CurriSection;
